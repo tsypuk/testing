630bbd3c6d5d6c76cbf5e004a414715812055bb6
Igor Torba <garrytrue@yandex.ru>
2017-03-12 13:48:19 +0200

    Add TestUtils

---
+++ src/test/java/com/garrytrue/TestUtils.java
@@ -0,0 +1,10 @@
+package com.garrytrue;
+
+/**
+ * Created by garrytrue on 12.03.17.
+ */
+public final class TestUtils {
+    private TestUtils() {
+        throw new AssertionError("Don't make instance");
+    }
+}
