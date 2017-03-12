3606558f887f15f2aa59a445c676516e998c6683
Igor Torba <garrytrue@yandex.ru>
2017-03-12 13:54:24 +0200

    Upd TestUtils

--- src/test/java/com/garrytrue/TestUtils.java
+++ src/test/java/com/garrytrue/TestUtils.java
@@ -1,5 +1,7 @@
 package com.garrytrue;
 
+import com.garrytrue.model.Student;
+
 /**
  * Created by garrytrue on 12.03.17.
  */
@@ -7,4 +9,9 @@ public final class TestUtils {
     private TestUtils() {
         throw new AssertionError("Don't make instance");
     }
+
+    public static Student getStudent1() {
+        return new Student(1, "One", "One");
+    }
+
 }
