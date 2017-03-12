ba3501adaa7c2f060438463322bd03fbd8f74b0c
Igor Torba <garrytrue@yandex.ru>
2017-03-12 17:05:32 +0200

    Add rest repository

---
+++ src/main/java/com/garrytrue/repository/StudentRESTRepository.java
@@ -0,0 +1,43 @@
+package com.garrytrue.repository;
+
+import com.garrytrue.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by garrytrue on 12.03.17.
+ */
+public class StudentRESTRepository implements CRUDRepository<Student> {
+    private final List<Student> list = new ArrayList<>();
+    private final AtomicLong atomicLong = new AtomicLong();
+
+    @Override
+    public Student save(Student data) {
+        long id = atomicLong.incrementAndGet();
+        data.setId(id);
+        list.add(data);
+        return data;
+    }
+
+    @Override
+    public Student get(long id) {
+        return list.stream().filter(student -> student.getId() == id).findFirst().orElse(null);
+    }
+
+    @Override
+    public Student update(Student data) {
+        return data;
+    }
+
+    @Override
+    public void delete(Student data) {
+        list.remove(data);
+    }
+
+    @Override
+    public void deleteAll() {
+        list.clear();
+    }
+}
